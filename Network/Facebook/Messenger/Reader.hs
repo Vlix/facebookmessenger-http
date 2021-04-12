6355c5324f80182aec928e26a545efb4c68d1d28
Felix Paulusma <fpaulusma@zig.nl>
2021-04-12 18:43:25 +0200

    same but in 'N.FB.M.Reader'

--- Network/Facebook/Messenger/Reader.hs
+++ Network/Facebook/Messenger/Reader.hs
@@ -12,13 +12,18 @@ module Network.Facebook.Messenger.Reader (
   , ParseError (..)
   -- * Requests
   , messageRequest
+  , messageRequest'
   , senderActionRequest
+  , senderActionRequest'
   -- ** User Profile
   , userProfileRequest
+  , userProfileRequest'
   , UserProfileType (..)
   -- ** Account Linking
   , psidRequest
+  , psidRequest'
   , accountUnlinkRequest
+  , accountUnlinkRequest'
   , AccountLinkToken (..)
 
 {-
@@ -54,6 +59,7 @@ module Network.Facebook.Messenger.Reader (
 import Control.Monad.Catch (MonadThrow)
 import Control.Monad.IO.Class (MonadIO)
 import Control.Monad.Reader (MonadReader, asks)
+import Data.Version (Version)
 
 import Network.HTTP.Client (HasHttpManager(..), Manager)
 
@@ -69,6 +75,15 @@ messageRequest :: (MonadThrow m, MonadIO m, HasHttpManager env, MonadReader env 
 messageRequest sRequest =
     withManager . NFB.messageRequest sRequest
 
+-- | Same as 'messageRequest', except you can set the version of the Send API
+messageRequest' :: (MonadThrow m, MonadIO m, HasHttpManager env, MonadReader env m)
+                => FB.SendRequest
+                -> Version
+                -> AccessToken
+                -> m (Response FB.MessageResponse FB.ErrorDetails)
+messageRequest' sRequest version =
+    withManager . NFB.messageRequest' sRequest version
+
 -- | The same as 'NFB.senderActionRequest', but gets the HTTP 'Manager' from the environment.
 senderActionRequest :: (MonadThrow m, MonadIO m, HasHttpManager env, MonadReader env m)
                     => FB.SenderActionRequest
@@ -76,6 +91,15 @@ senderActionRequest :: (MonadThrow m, MonadIO m, HasHttpManager env, MonadReader
                     -> m (Response FB.SenderActionResponse FB.ErrorDetails)
 senderActionRequest saRequest =
     withManager . NFB.senderActionRequest saRequest
+
+-- | Same as 'senderActionRequest', except you can set the version of the Send API
+senderActionRequest' :: (MonadThrow m, MonadIO m, HasHttpManager env, MonadReader env m)
+                     => FB.SenderActionRequest
+                     -> Version
+                     -> AccessToken
+                     -> m (Response FB.SenderActionResponse FB.ErrorDetails)
+senderActionRequest' saRequest version =
+    withManager . NFB.senderActionRequest' saRequest version
 
 -- | The same as 'NFB.userProfileRequest', but gets the HTTP 'Manager' from the environment.
 userProfileRequest :: (MonadThrow m, MonadIO m, HasHttpManager env, MonadReader env m)
@@ -86,6 +110,16 @@ userProfileRequest :: (MonadThrow m, MonadIO m, HasHttpManager env, MonadReader 
 userProfileRequest uptypes userid =
     withManager . NFB.userProfileRequest uptypes userid
 
+-- | Same as 'userProfileRequest', except you can set the version of the Send API
+userProfileRequest' :: (MonadThrow m, MonadIO m, HasHttpManager env, MonadReader env m)
+                    => [UserProfileType]
+                    -> FB.PSID
+                    -> Version
+                    -> AccessToken
+                    -> m (Response FB.UserProfileResponse FB.ErrorDetails)
+userProfileRequest' uptypes userid version =
+    withManager . NFB.userProfileRequest' uptypes userid version
+
 -- | The same as 'NFB.psidRequest', but gets the HTTP 'Manager' from the environment.
 psidRequest :: (MonadThrow m, MonadIO m, HasHttpManager env, MonadReader env m)
             => AccountLinkToken
@@ -94,6 +128,15 @@ psidRequest :: (MonadThrow m, MonadIO m, HasHttpManager env, MonadReader env m)
 psidRequest accountLinkToken =
     withManager . NFB.psidRequest accountLinkToken
 
+-- | Same as 'psidRequest', except you can set the version of the Send API
+psidRequest' :: (MonadThrow m, MonadIO m, HasHttpManager env, MonadReader env m)
+             => AccountLinkToken
+             -> Version
+             -> AccessToken
+             -> m (Response FB.AccountLinkingResponse FB.ErrorDetails)
+psidRequest' accountLinkToken version =
+    withManager . NFB.psidRequest' accountLinkToken version
+
 -- | The same as 'NFB.accountUnlinkRequest', but gets the HTTP 'Manager' from the environment.
 accountUnlinkRequest :: (MonadThrow m, MonadIO m, HasHttpManager env, MonadReader env m)
                      => FB.AccountUnlinkRequest
@@ -101,6 +144,15 @@ accountUnlinkRequest :: (MonadThrow m, MonadIO m, HasHttpManager env, MonadReade
                      -> m (Response FB.SuccessResponse FB.ErrorDetails)
 accountUnlinkRequest auRequest =
     withManager . NFB.accountUnlinkRequest auRequest
+
+-- | Same as 'accountUnlinkRequest', except you can set the version of the Send API
+accountUnlinkRequest' :: (MonadThrow m, MonadIO m, HasHttpManager env, MonadReader env m)
+                      => FB.AccountUnlinkRequest
+                      -> Version
+                      -> AccessToken
+                      -> m (Response FB.SuccessResponse FB.ErrorDetails)
+accountUnlinkRequest' auRequest version =
+    withManager . NFB.accountUnlinkRequest' auRequest version
 
 withManager :: (HasHttpManager env, MonadReader env m)
             => (Manager -> m a)
